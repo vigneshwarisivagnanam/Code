46a829356740c43d56f0f1061b2693e7f65109ba
Training_B8C.07.19 <Training_B8C.07.19@BDC8-DX-6PKDH92.dir.svc.accenture.com>
2018-07-18 12:47:56 +0530

    commit

---
+++ src/test/java/Test2/ProjectB.java
@@ -0,0 +1,23 @@
+package Test2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ProjectB {
+  @Test
+  public void testlogin() 
+  {
+	  System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver_win32\\chromedriver.exe");
+	  WebDriver driver = new ChromeDriver ();
+			  driver.get("http://www.google.com");
+	  WebElement element = driver.findElement(By.name("q"));
+	  element.sendKeys("Current sport news");
+	  element.submit();
+	  System.out.println("hi");
+	  System.out.println("hi");
+  }
+  }
+  
---
+++ src/test/java/Test2/Project_GITHUB.java
@@ -0,0 +1,45 @@
+package Test2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+public class Project_GITHUB {
+	@Parameters("browser")
+	@Test
+	public void testlogin(String value)
+	{
+		WebDriver driver=null;
+		if(value.equals("chrome"))
+		{
+		 System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver_win32\\chromedriver.exe");
+		  driver = new ChromeDriver ();
+		}
+		else if(value.equals("firefox"))
+				{
+			 System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\chromedriver_win32\\geckodriver.exe");
+			  driver = new FirefoxDriver ();
+			}
+		else {
+			System.out.println("No Driver Found");
+		}
+				  driver.get("https://github.com/login");
+				  driver.findElement(By.id("login_field")).sendKeys("vigneshwarisivagnanam");
+				  driver.findElement(By.id("password")).sendKeys("Vigneshwari@123");
+				  driver.findElement(By.name("commit")).click();
+				  String title=driver.getTitle();
+				  System.out.println(title);
+				  
+				  Assert.assertEquals(title,"GitHub");
+				  
+				  System.out.println("verified");
+								  //**mkfsdasadsazczcbvxsczvxvxzdcfdz
+				  
+	}
+
+}
